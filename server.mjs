67507b7b6b861a5183a01b8de53d5bec7898b23e
Aamir Nakhwa <aamirnakhwa@yahoo.com>
2023-09-27 10:24:50 +0500

    Build removed

--- server.mjs
+++ server.mjs
@@ -52,7 +52,7 @@ const indexName = process.env.PINECONE_INDEX_NAME;
 const index = pinecone.Index(indexName);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors(["http://localhost:3000", "127.0.0.1", "*"]));
